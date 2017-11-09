37c3ef05f7df5fe93e7450fe0ae3422da63cd4a5
Gimantha Bandara <gimantha@wso2.com>
2017-11-09 21:35:01 +0530

    Adding a missing method to config DAS -602
    (cherry picked from commit 855de6a)

--- components/analytics-core/org.wso2.carbon.analytics.dataservice.core/src/main/java/org/wso2/carbon/analytics/dataservice/core/config/AnalyticsFacetConfiguration.java
+++ components/analytics-core/org.wso2.carbon.analytics.dataservice.core/src/main/java/org/wso2/carbon/analytics/dataservice/core/config/AnalyticsFacetConfiguration.java
@@ -73,4 +73,8 @@ public class AnalyticsFacetConfiguration {
     public AnalyticsFacetTableConfiguration[] getFacetTableConfigurations() {
         return facetTableConfigurations;
     }
+
+    public void setFacetTableConfigurations(AnalyticsFacetTableConfiguration[] facetTableConfigurations) {
+        this.facetTableConfigurations = facetTableConfigurations;
+    }
 }
